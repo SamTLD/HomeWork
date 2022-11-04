6293c3cf7e2c9e4170fc065ad4fb66acdc7af82d
SamTLD <Samitha.tld@gmail.com>
2022-11-04 15:35:41 +0530

    bad

---
+++ 15-npm-intro.js
@@ -0,0 +1,20 @@
+//npm -global command, comes with node
+//npm --vertion, npm --v
+
+//local dependency -  use it only in your project
+//npm i <packageName>
+
+//global dependency - use it in any project
+//npm install -g <packageName>
+//sudo npm install -g <packageName> (mac)
+
+//package.json - manifest file (store importent info abnout projct/package)
+//manual approach (create package.json in the root, create properties etc)
+//npm init (step by step, press enter to skip)
+//npm init -y (everything is defolut)
+
+const name = require('lodash')
+const items =[1,[2,[3,[4]]]]
+const newItems = name.flattenDeep(items)
+
+console.log(newItems)
