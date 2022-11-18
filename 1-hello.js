28b589bc101e9dfdcf502ad43a1dae65842bd1d6
SamTLD <Samitha.tld@gmail.com>
2022-11-18 15:54:34 +0530

    11/18

--- 1-hello.js
+++ 1-hello.js
@@ -1,11 +1,18 @@
 let value = 5;
-console.log('Is this true');
+console.log('Is this true 5 > 10');
 if (value>10)
-console.log('True')
+console.log('no')
 else 
-console.log('Low')
+console.log('yes')
 
 
+
+let number = 120;
+console.log('Is this true 120>20')
+if (number>120)
+console.log('No')
+else
+console.log('yes')
 
 
 
--- 2-globle.js
+++ 2-globle.js
@@ -6,9 +6,26 @@
 "module (into about current module)"
 "process (into about err)"
 
-*/
+
 console.log (module)
 
 setInterval(()=>{
     console.log("100")
 },1000)
+
+setTimeout(()=>{
+    console.log('sam')
+},2000)
+
+setInterval(()=>{
+    console.log('sami')
+},10000)
+
+
+setTimeout(()=>{
+    console.log('sam')
+},5000)*/
+
+console.log(process)
+
+
